be3fe39ca3477c944b8fca06b0e459e7293ac7b8
achoi616 <achoi616@gmail.com>
2020-06-15 18:15:41 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,14 +1,4 @@
-def introduction(Alex)
-  puts "Hi, my name is #{name}."
-end
 
-def introduction_with_language(Alex, Java)
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
-
-def introduction_with_language_optional(name, language = "Ruby")
-  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-end
 
 
 
